86648ac1f4c45677cc5ba88e75d003b3b9e72061
Myke Maina <0devmichael@gmail.com>
2023-09-01 02:23:33 +0300

    Minor change

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
 
   return (
-    <main className=" relative overflow-hidden  bg-[#252525] text-white px-20 py-[8rem]">
-      <div className="shadow-[#252525] shadow-2xl w-[12rem]  h-[12rem] absolute rounded-full bg-[#AC8D75] left-[35%] top-20 z-[9]"></div>
+    <main className=" relative overflow-hidden  bg-[#252525] text-white px-20 py-[5rem]">
+      <div className="shadow-[#252525] shadow-2xl w-[12rem]  h-[12rem] absolute rounded-full bg-[#AC8D75] left-[35%] top-[3rem] z-[9]"></div>
       <div className="shadow-[#252525] shadow-2xl w-[20rem]  h-[20rem] absolute rounded-full bg-[#AC8D75] -right-10 bottom-0 z-[9]"></div>
       <div className="shadow-[#252525] shadow-2xl w-[4rem]  h-[4rem] absolute rounded-full bg-[#AC8D75] left-10 top-[50%] z-[999]"></div>
       <div className="flex flex-col justify-between h-screen z-[99] relative bg-hero bg-center bg-no-repeat bg-cover rounded-xl">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,15 @@ const Home = () => {
   return (
     <>
       <div className="relative flex items-center justify-between w-full px-20">
-        <div className="absolute inset-0 bg-black z-[-1] opacity-[.7]"></div>
+        <div className="absolute inset-0 bg-[#000] blur z-[-1] opacity-[.7] motion-safe:animate-pulse"></div>
         <div className="w-full space-y-6">
-          <h2 className="font-[900] font-[Capriola] uppercase">
+          <h2 className="font-[900] font-[Capriola] uppercase bg-gradient-to-tr from-[#C99C3E] via-[#9b7df2] to-[#9C84E1] bg-clip-text text-transparent">
             take your chess game to the next level
           </h2>
-          <h5>
+          <p>
             Connect with top ranked chess players from around the world and
             learn the secrets to becoming a chess master
-          </h5>
+          </p>
           <button className="px-4 py-2 capitalize border shadow-2xl rounded-2xl shadow-[#888]">
             become a master
           </button>
