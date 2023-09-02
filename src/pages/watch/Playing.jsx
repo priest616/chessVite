cda287ef87d8cadc3fef73873ec7ef8de3bec6a1
Myke Maina <0devmichael@gmail.com>
2023-09-03 02:44:41 +0300

    added all apis

---
+++ src/pages/watch/Playing.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const ChessMatch = () => {
+  const [matchData, setMatchData] = useState({});
+
+  useEffect(() => {
+    const fetchMatchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.chess.com/pub/match/12803/1"
+        );
+        setMatchData(response.data);
+      } catch (error) {
+        console.error("Error fetching match data:", error);
+      }
+    };
+
+    fetchMatchData();
+  }, []);
+
+  return (
+    <div className="container mx-auto mt-8">
+      <h1 className="mb-4 text-2xl font-semibold">Chess.com Match</h1>
+      <div className="mb-4">
+        <h2 className="text-lg font-semibold">Match Details</h2>
+        <ul>
+          <li>
+            ID: {matchData.id}
+          </li>
+          <li>
+            URL: <a href={matchData.url} target="_blank" rel="noopener noreferrer">{matchData.url}</a>
+          </li>
+          <li>
+            Time Control: {matchData.time_control}
+          </li>
+          <li>
+            End Time: {matchData.end_time}
+          </li>
+          {/* Add more details as needed */}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ChessMatch;
--- src/pages/watch/Watch.jsx
+++ src/pages/watch/Watch.jsx
@@ -1,51 +1,28 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { ImArrowUpRight } from "react-icons/im";
+import { BsDot } from "react-icons/bs";
+import Playing from "./Playing";
 
 const Watch = () => {
-  const [watchData, setWatchData] = useState([]);
-  const [activeOption, setActiveOption] = useState(0); 
+  const [watchData, setWatchData] = useState({ streamers: [] });
+  const [activeOption, setActiveOption] = useState(0);
   const [apiURL, setApiURL] = useState("");
-const stream = [
-   { "streamers": [
-        {
-            "username": "HunterWhiz",
-            "avatar": "https://images.chesscomfiles.com/uploads/v1/user/162313815.8734aafa.50x50o.709e5205a588.jpeg",
-            "twitch_url": "https://twitch.tv/hunterwhiz",
-            "url": "https://www.chess.com/member/HunterWhiz",
-            "is_live": true,
-            "is_community_streamer": false
-        },
-        {
-            "username": "TheMagician",
-            "avatar": "https://images.chesscomfiles.com/uploads/v1/user/1762234.e9232887.50x50o.bd8ae2ae8eb1.jpeg",
-            "twitch_url": "https://twitch.tv/fmjohncurtischess",
-            "url": "https://www.chess.com/member/TheMagician",
-            "is_live": true,
-            "is_community_streamer": false
-        },
-        {
-            "username": "KingsBishop",
-            "avatar": "https://images.chesscomfiles.com/uploads/v1/user/17669546.18317e7e.50x50o.cee23d3e3050.jpeg",
-            "twitch_url": "https://twitch.tv/kingsbischop",
-            "url": "https://www.chess.com/member/KingsBishop",
-            "is_live": true,
-            "is_community_streamer": false
-        },
-    ]
+  const [showMore, setShowMore] = useState(10);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(apiURL);
+      setWatchData(response.data);
+    } catch (error) {
+      console.error(error);
     }
-]
-console.log(watchData)
+  };
+
   useEffect(() => {
-    const fetchPuzzle = async () => {
-      try {
-        const response = await axios.get(apiURL);
-        setWatchData(response.data);
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchPuzzle();
+    if (apiURL) {
+      fetchData();
+    }
   }, [apiURL]);
 
   const watchOptions = [
@@ -55,45 +32,105 @@ console.log(watchData)
     },
     {
       name: "Playing Now",
-      url: "https://api.chess.com/pub/puzzle/random",
-    },
-    {
-      name: "Events",
-      url: "https://api.chess.com/pub/puzzle/random",
+      url: "https://api.chess.com/pub/match/12803/1",
     },
   ];
 
+  const tableTitles = [
+    {
+      name: "Icon",
+    },
+    {
+      name: "Streamer",
+    },
+    {
+      name: "Profile",
+    },
+    {
+      name: "Status",
+    },
+  ];
+
+  const handleShowMore = () => {
+    setShowMore((prevShowMore) => prevShowMore + 10);
+  };
+
   return (
-    <div>
-      {stream.streamers && (stream.streamers.map((item, index) => (
-        <div key={index} className="flex">
-          <div key={index}>
-            {item.username}
-            {item.status}
-          </div>
-          <img src={item.image} alt="" />
-          <div>{item.gender}</div>
-        </div>
-      )))}
-      <>
-        <div className="flex gap-5">
-          {watchOptions.map((option, index) => (
-            <React.Fragment key={index}>
-              <button
-                className={`px-5 py-3 border rounded-md ${
-                  activeOption === index ? "bg-[#81B64C]" : ""
-                }`}
-                onClick={() => {
-                  setActiveOption(index);
-                  setApiURL(option.url);
-                }}
-              >
-                <p>{option.name}</p>
-              </button>
-            </React.Fragment>
-          ))}
-        </div>
-      </>
+    <div className="container mx-auto mt-8">
+      <div className="absolute inset-0 bg-black opacity-[.7] -z-[1]"></div>
+      <div className="flex justify-around mb-4">
+        {watchOptions.map((option, index) => (
+          <button
+            key={index}
+            className={`px-5 py-3 border rounded-md ${
+              activeOption === index ? "bg-[#81B64C]" : ""
+            }`}
+            onClick={() => {
+              setActiveOption(index);
+              setApiURL(option.url);
+            }}
+          >
+            {option.name}
+          </button>
+        ))}
+      </div>
+      {activeOption === 0 && (
+        <>
+          {watchData.streamers.length > 0 && (
+            <div className="w-1/2 mx-auto">
+              <div className="grid grid-cols-4 py-2 bg-gray-700">
+                {tableTitles.map((title, index) => (
+                  <div key={index} className="p-2 font-bold text-center">
+                    {title.name}
+                  </div>
+                ))}
+              </div>
+              {watchData.streamers &&
+                watchData.streamers.length > 0 &&
+                watchData.streamers.slice(0, showMore).map((item, index) => (
+                  <div
+                    key={index}
+                    className={`grid grid-cols-4 items-center ${
+                      index % 2 === 0 ? "bg-gray-900" : "bg-gray-700"
+                    } py-2 border-b border-gray-300`}
+                  >
+                    <div className="col-span-1 p-2">
+                      <img
+                        src={item.avatar}
+                        className="w-12 h-12 mx-auto rounded-full"
+                        alt=""
+                      />
+                    </div>
+                    <div className="col-span-1 p-2">{item.username}</div>
+                    <div className="col-span-1 p-2">
+                      <a
+                        href={item.url}
+                        className="flex items-center gap-4 overflow-hidden"
+                      >
+                        Profile URL <ImArrowUpRight className="text-xs" />
+                      </a>
+                    </div>
+                    <div className="flex items-center col-span-1 p-2">
+                      <BsDot className="text-green-500 animate-pulse text-[3rem]" />{" "}
+                      Online
+                    </div>
+                  </div>
+                ))}
+              {watchData.streamers && showMore < watchData.streamers.length && (
+                <div className="mt-4 text-center">
+                  <button
+                    className="px-4 py-2 rounded-md bg-[#81B64C] text-white"
+                    onClick={handleShowMore}
+                  >
+                    View More
+                  </button>
+                </div>
+              )}
+            </div>
+          )}
+        </>
+      )}
+      {activeOption === 1 && <Playing />}
     </div>
   );
 };
